89296b1b3ddaf4808931dc1d58b1b6e03df8dec6
advaittinaikar <advait.tinaikar>
2016-12-10 17:59:13 -0500

    helper error commit

--- helpers/commands_helper.rb
+++ helpers/commands_helper.rb
@@ -34,17 +34,14 @@ module Sinatra
 
       elsif event.formatted_text.starts_with? "thank"
         client.chat_postMessage(channel: event.channel, text: "That's mighty nice of you. You're welcome and thank you for having me!", as_user: true)
-
-      #elsif event.formatted_text.starts_with? "add"
         
-                             
       else
         # ERROR Commands
         # not understood or an error
         error_counter += 1
 
         if error_counter (1..5)
-          client.chat_postMessage(channel: event.channel, text: "I didn't get that but that's alright If you're stuck, type `help` to find my commands.", as_user: true)
+          client.chat_postMessage(channel: event.channel, text: "I didn't get that but that's alright. If you're stuck, type `help` to find my commands.", as_user: true)
         elsif error_counter (6..10)
           client.chat_postMessage(channel: event.channel, text: "Hmmm, you seems to be different today. Hope all is well. Anyways, type `help` to find my commands.", as_user: true)  
         else  
