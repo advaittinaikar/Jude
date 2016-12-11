7154e1e6266ab32c4f083c39649fc6ba6c289b9f
advaittinaikar <advait.tinaikar>
2016-12-11 17:40:22 -0500

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'active_support/all'
 
-Course.create!([{ course_name: "Design for the Environment", course_id: "49-100" } ])
-Course.create!([{ course_name: "Programming for Online Prototypes", course_id: "49-101" } ])
-Course.create!([{ course_name: "Visual Processes", course_id: "49-102" } ])
+Course.create!([{ course_name: "Design for the Environment", course_id: "49-100" },
+				{ course_name: "Programming for Online Prototypes", course_id: "49-101" },
+				{ course_name: "Visual Processes", course_id: "49-102" } ])
