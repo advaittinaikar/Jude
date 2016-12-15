8c8b8bb9712a881461b97a9d508408569c92d9a5
advaittinaikar <advait.tinaikar>
2016-12-15 13:01:45 -0500

    delete previous course entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'active_support/all'
 
+Course.delete_all
+
 Course.create!([{ course_name: "Design for the Environment", course_id: "49-100", short_name: "DFE", instructor: "Steve Leonard" },
 				{ course_name: "Programming for Online Prototypes", course_id: "49-101", short_name: "POP", instructor: "Daragh Bryne"},
 				{ course_name: "Visual Processes", course_id: "49-102", short_name: "VP", instructor: "Eric Anderson"} ])
