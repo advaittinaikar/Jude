977f81225127632fda5f4fd4d62d87cf128b88a1
advaittinaikar <advait.tinaikar>
2016-12-15 15:09:04 -0500

    calendar scope

--- helpers/calendar_helper.rb
+++ helpers/calendar_helper.rb
@@ -26,6 +26,7 @@ module Sinatra
 		    client_id: ENV['CALENDAR_CLIENT_ID'],
 		    client_secret: ENV['CALENDAR_CLIENT_SECRET'],
 		    authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+        scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
 		    redirect_uri: "https://agile-stream-68169.herokuapp.com/oauthcallback"
 	  	})
 
