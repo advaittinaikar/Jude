ab16d905f50d29c138b597ab65525cf9ee85a848
advaittinaikar <advait.tinaikar>
2016-12-27 22:14:32 +0530

    checking team

--- app.rb
+++ app.rb
@@ -123,13 +123,13 @@ get "/oauth" do
       if team["calendar_token"].nil?
         auth_calendar
       else
-        sign_up_greeting
+        sign_up_greeting team
       end
 
     else
-      sign_up_greeting
+      sign_up_greeting team
     end
-    
+
   else
     401
   end
@@ -166,7 +166,7 @@ get '/oauthcallback' do
     if response
       team.calendar_token = response['access_token']
       # finally respond...
-      sign_up_greeting
+      sign_up_greeting team
     else
       "Something went wrong in setting up your calendar and slack.\nWe'd appreciate it if you could try again!" 
     end
@@ -403,6 +403,6 @@ def respond_to_slack_event json
   
 end
 
-def sign_up_greeting
-  "Jude has been successfully installed.\nYour Calendar has been already been synced with Jude.\nPlease login to your Slack team to meet Jude!"
+def sign_up_greeting team
+  "#{team}\nJude has been successfully installed.\nYour Calendar has been already been synced with Jude.\nPlease login to your Slack team to meet Jude!"
 end
--- helpers/calendar_helper.rb
+++ helpers/calendar_helper.rb
@@ -108,10 +108,7 @@ module Sinatra
     access_token = team["calendar_token"]
     access_code = team["calendar_code"]
 
-    client = Signet::OAuth2::Client.new( 
-      { access_token: access_token, 
-        token_credential_uri:  'https://accounts.google.com/o/oauth2/token'} 
-      )
+    client = Signet::OAuth2::Client.new( access_token: access_token )
 
     client.update!(
       :code => access_code,
