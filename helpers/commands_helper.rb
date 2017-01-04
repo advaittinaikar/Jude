78f50d4be0c5907a6dea4626dfb085e2940a5c49
advaittinaikar <advait.tinaikar>
2017-01-04 19:30:26 +0530

    error testing for events

--- helpers/commands_helper.rb
+++ helpers/commands_helper.rb
@@ -30,7 +30,7 @@ module Sinatra
       user_events = Event.find_by(user_id: user_id)
       second_last_event = user_events[-2]
       
-      puts second_last_event
+      puts "Event is #{second_last_event}, user_events is #{user_events} and user_id is #{user_id}"
 
       # Hi Commands
       if ["hi","hello","hey","heyy"].any? { |w| ef.starts_with? w }
@@ -99,13 +99,13 @@ module Sinatra
 
         client.chat_postMessage(channel: event.channel, text: show_courses, as_user: true)
 
-      elsif second_last_event.direction == "outgoing" && second_last_event.text == "add assignment description"
+      elsif second_last_event["direction"] == "outgoing" && second_last_event["text"] == "add assignment description"
 
           $assignment.description = input
           client.chat_postMessage(channel: second_last_event.channel, text: "So when is this assignment due?", as_user: true)
           add_outgoing_event team, "message", "add assignment due-date"
 
-      elsif second_last_event.direction == "outgoing" && second_last_event.text == "add assignment due-date"
+      elsif second_last_event["direction"] == "outgoing" && second_last_event["text"] == "add assignment due-date"
           
           due_date = Kronic.parse(input)
 
